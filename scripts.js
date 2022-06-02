e03aef5ec203fa0cc88cc27e68fd71680a23616a
Hans Janowitz <hans.janowitz@rga.com>
2022-06-02 10:54:33 -0700

    question styling and initial js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,31 @@
 window.addEventListener('load', function () {
+  let question = document.querySelector(".question");
+  let answer = document.querySelector(".answer");
   let exitButton = document.querySelector(".get-away");
+  let start = document.querySelector(".start");
+  let startButton = document.querySelector(".start-button");
+  let nextButton = document.querySelector(".next-button");
 
   function getAway() {
-    // Get away right now
+    // Open new tab with generic safe site content
     window.open("http://weather.com", "_newtab");
-    // Replace current site with another benign site
+    // Replace current site with another safe site
     window.location.replace('http://google.com');
   }
 
   exitButton.addEventListener("click", getAway);
+  startButton.addEventListener("click", startQuiz);
+  nextButton.addEventListener("click", advanceQuiz);
+  
+  function startQuiz() {
+    start.classList.add("hidden");
+    nextButton.classList.remove("hidden");
+    question.textContent="The question is somewhat long so it will take up multiple lines and go on and on and not stop after just a few letters, etc. So here we are seeing multiple sentences.";
+    question.classList.remove("hidden");
+    answer.classList.remove("hidden");
+  }
+
+  function advanceQuiz() {
+
+  }
 })
